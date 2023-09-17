c53226c60453bfbb1d40e5778690b3a2acf243e1
Yuu <117032755+serafyon@users.noreply.github.com>
2023-09-18 07:42:27 +0800

    ASCII art
    simple ASCII art via java print.

---
+++ MyClass.java
@@ -0,0 +1,26 @@
+public class MyClass {
+    public static void main(String args[]) {
+        //cat
+      System.out.println("                /\\____/\\  ,__,,,___,,");
+      System.out.println("               / \" \" \"  \\/           \\");
+      System.out.println("             =| o ㅅ  o  |=           |");
+      System.out.println("               \\        /  _           |");
+      System.out.println("               |_    _,,  ,,   _____,'' }");
+      System.out.println("              (  =)(=     ( _________,,,/");
+        // on a TV
+      System.out.println("          /¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯\\");
+      System.out.println("          |  _____________________________  |");
+      System.out.println("          | |                             | |");
+      System.out.println("          | |                             | |");
+      System.out.println("          | |                             | |");
+      System.out.println("          | |                             | |");
+      System.out.println("          | |                             | |");
+      System.out.println("          | |                             | |");
+      System.out.println("          | |_____________________________| |");
+      System.out.println("          |                                 |");
+      System.out.println("          |                        {} o o   |");
+      System.out.println("          \\_________________________________/  FVH");
+      
+      
+    }
+}
